a3f19fa45d1ae4cd16d71f5ce3ed2317e43b9b48
Matej Šrubař <matosrubar@gmail.com>
2018-05-20 19:42:44 +0200

    create Page model

--- datracker/admin.py
+++ datracker/admin.py
@@ -2,7 +2,7 @@ from djsingleton.admin import SingletonAdmin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-from datracker.models import Employee, SiteSettings
+from datracker.models import Employee, Page, SiteSettings
 
 
 @admin.register(Employee)
@@ -12,3 +12,7 @@ class EmployeeAdmin(UserAdmin):
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(SingletonAdmin):
     model = SiteSettings
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    model = Page
--- datracker/models.py
+++ datracker/models.py
@@ -1,4 +1,5 @@
 from djsingleton.models import SingletonModel
+from django_extensions.db.models import TimeStampedModel
 
 from django.contrib.auth.models import User, AbstractUser
 from django.db import models
@@ -26,4 +27,30 @@ class SiteSettings(SingletonModel):
     def __str__(self):
         return '<{} #{} {}>'.format(
             self.__class__.__name__, self.pk, self.site_name
+        )
+
+
+class Page(TimeStampedModel):
+    title = models.CharField(
+        max_length=150, help_text='Main title displayed on top of each page', default=''
+    )
+    label = models.CharField(
+        max_length=150, help_text='Is displayed for example in navbar or links', default=''
+    )
+    meta_title = models.CharField(
+        max_length=150, help_text='Title used only for social networks and search engines.', default=''
+    )
+    meta_description = models.TextField(
+        max_length=200,
+        help_text='Description used only for social networks and search engines.',
+        default=''
+    )
+    content = models.TextField(default='')
+    slug = models.SlugField(
+        max_length=50, help_text='Specifies name of page in URL address.', unique=True
+    )
+
+    def __str__(self):
+        return '<{} #{} {}>'.format(
+            self.__class__.__name__, self.pk, self.title[:20]
         )
