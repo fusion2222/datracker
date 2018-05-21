18af9f8acfad21a230fd0701532e2b7e48b66514
Matej Šrubař <matosrubar@gmail.com>
2018-05-21 02:46:21 +0200

    text response changed to template response

--- datracker/views.py
+++ datracker/views.py
@@ -1,4 +1,4 @@
-from django.template.response import HttpResponse
+from django.template.response import TemplateResponse
 
 def index(request):
-    return HttpResponse('Works perfectly!')
+    return TemplateResponse(request, 'base.html', {"title": "test"})
