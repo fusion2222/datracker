0dedf2a6deb662b2706cd4ca0a450549f758a936
Matej Šrubař <matosrubar@gmail.com>
2018-05-21 03:54:53 +0200

    added idle logout link to navbar

--- datracker/context_processors.py
+++ datracker/context_processors.py
@@ -1,9 +1,13 @@
 from datracker.models import Page
 
+
 def navbar_menu(request):
 
     pages = Page.objects.all().order_by('pk')
 
-    output = [{'label': page.label, 'url': '#get_absolute_link'} for page in pages]
+    output = [{'label': page.label, 'url': page.get_absolute_url()} for page in pages]
+
+    if request.user.is_authenticated:
+        output.append({'label': 'Logout', 'url': 'admin/logout'})
 
     return {'navbar_links': output}
